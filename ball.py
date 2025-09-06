678b6192ad5988828f41f1ff33c0391f9986e939
jomrembau <jomir.bautista@gmail.com>
2025-09-06 14:43:38 +0200

    Update ball.py

--- ball.py
+++ ball.py
@@ -11,6 +11,7 @@ class Ball:
         self.ball.goto(0,0)
         self.initial_angle = 45
         self.ball.left(self.initial_angle)
+        self.ball_speed = 15
 
     def move_ball(self):
 
@@ -19,6 +20,16 @@ class Ball:
         if abs(ball_y) >= 190:
             heading = self.ball.heading()
             self.ball.setheading( - heading)
-        self.ball.fd(15)
+        self.ball.fd(self.ball_speed)
 
 
+    def reset_ball(self):
+        heading = self.ball.heading()
+        self.ball.goto(0,0)
+        self.ball_speed += 1
+        self.ball.fd(self.ball_speed)
+        self.ball.setheading(180- heading)
+
+    def game_over(self):
+        self.ball.goto(0, 0)
+        self.ball.write("G A M E  O V E R", font=("Arial", 12, ""), align="center")
