4188f010e0298bcc8f79af38fcaab53d01e998c0
jomrembau <jomir.bautista@gmail.com>
2025-09-06 14:44:31 +0200

    Update score.py

--- score.py
+++ score.py
@@ -2,12 +2,16 @@ from turtle import Turtle
 
 class Score:
 
-    def __init__(self, x_pos,y_pos, current_score):
+    def __init__(self, x_pos,y_pos):
         self.score = Turtle()
         self.score.hideturtle()
         self.score.penup()
         self.score.color("white")
         self.score.goto(x_pos,y_pos)
         self.score.write("")
-        self.current_score = current_score
-        self.score.write(current_score, font=("Arial", 12, ""), align="center")
+        self.current_score = 0
+        self.score.write(self.current_score, font=("Arial", 12, ""), align="center")
+
+    def update_score(self):
+        self.score.clear()
+        self.score.write(self.current_score, font=("Arial", 12, ""), align="center")
